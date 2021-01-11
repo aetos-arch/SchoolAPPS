ead5a0aa4edac3d53d90e43195e98419440cf2c8
KO-03 <58856041+KO-03@users.noreply.github.com>
2021-01-11 13:49:59 +0100

    Improve front + some text + updates pt 2

--- include/inc_articles.php
+++ include/inc_articles.php
@@ -1,71 +1,73 @@
 <div class="content-block" id="latest-articles">
     <h1>Nos articles</h1>
-    <section class="row">
-        <article class="articles col-md-4">
-            <h4>Titre de l'article</h4>
-            <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
-            <div class="row">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
-                </p>
+    <div class="container-fluid">
+        <section class="row">
+            <div class="articles col-lg-3">
+                <h4>Titre de l'article</h4>
+                <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
+                <div class="row">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+                    </p>
+                </div>
+                <a class="btn lire-plus" href="">Lire plus</a>
             </div>
-            <a class="btn lire-plus" href="">Lire plus</a>
-        </article>
-        <article class="articles col-md-4">
-            <h4>Titre de l'article</h4>
-            <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
-            <div class="row">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
-                </p>
+            <div class="articles col-lg-3">
+                <h4>Titre de l'article</h4>
+                <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
+                <div class="row">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+                    </p>
+                </div>
+                <a class="btn lire-plus" href="">Lire plus</a>
             </div>
-            <a class="btn lire-plus" href="">Lire plus</a>
-        </article>
-        <article class="articles col-md-4">
-            <h4>Titre de l'article</h4>
-            <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
-            <div class="row">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
-                </p>
+            <div class="articles col-lg-3">
+                <h4>Titre de l'article</h4>
+                <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
+                <div class="row">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+                    </p>
+                </div>
+                <a class="btn lire-plus" href="">Lire plus</a>
             </div>
-            <a class="btn lire-plus" href="">Lire plus</a>
-        </article>
-        <article class="articles col-md-4">
-            <h4>Titre de l'article</h4>
-            <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
-            <div class="row">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
-                </p>
+            <div class="articles col-lg-3">
+                <h4>Titre de l'article</h4>
+                <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
+                <div class="row">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+                    </p>
+                </div>
+                <a class="btn lire-plus" href="">Lire plus</a>
             </div>
-            <a class="btn lire-plus" href="">Lire plus</a>
-        </article>
-        <article class="articles col-md-4">
-            <h4>Titre de l'article</h4>
-            <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
-            <div class="row">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
-                </p>
+            <div class="articles col-lg-3">
+                <h4>Titre de l'article</h4>
+                <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
+                <div class="row">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+                    </p>
+                </div>
+                <a class="btn lire-plus" href="">Lire plus</a>
             </div>
-            <a class="btn lire-plus" href="">Lire plus</a>
-        </article>
-        <article class="articles col-md-4">
-            <h4>Titre de l'article</h4>
-            <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
-            <div class="row">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
-                </p>
+            <div class="articles col-lg-3">
+                <h4>Titre de l'article</h4>
+                <img class="img-thumbnail" src="../images/200x200.png" alt="logo produit">
+                <div class="row">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Sit amet porttitor eget dolor morbi non arcu. Consectetur a erat nam at lectus urna duis. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+                    </p>
+                </div>
+                <a class="btn lire-plus" href="">Lire plus</a>
             </div>
-            <a class="btn lire-plus" href="">Lire plus</a>
-        </article>
-    </section>
+        </section>
+    </div>
 </div>
