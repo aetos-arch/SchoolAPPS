831272cb810d3819fc738a903e985506bf68d335
Emilie Coatelant <emiliecoatelant@outlook.fr>
2021-01-11 14:05:25 +0100

    Vue connexion

--- config/connexion.php
+++ config/connexion.php
@@ -11,8 +11,8 @@ class Connexion
     {
         $db_host = "localhost";
         $user = "root";
-        $password = "";
-        $db_name = "schoolapps";
+        $password = "root";
+        $db_name = "SchoolAPPS";
         $dns = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
         try {
             self::$bdd = new PDO($dns, $user, $password);
