14008545fc241e85b98125e18216046f31b1c1b4
Nicolas-Torres <sdtorres.sa@gmail.com>
2021-01-18 20:49:46 +0100

    images sponsors

--- include/inc_sponsors.php
+++ include/inc_sponsors.php
@@ -1,9 +1,9 @@
 <div class="container content-block">
     <h1>Nos Sponsors</h1>
     <section class="row sponsor">
-            <div class="col sponsor"><img class="rounded-2" src="/images/100x100.png" alt="First slide"></div>
-            <div class="col sponsor"><img class="rounded-2" src="/images/100x100.png" alt="First slide"></div>
-            <div class="col sponsor"><img class="rounded-2" src="/images/100x100.png" alt="First slide"></div>
-            <div class="col sponsor"><img class="rounded-2" src="/images/100x100.png" alt="First slide"></div>
+            <div class="col sponsor"><img class="rounded-2" src="/images/illustrations/iut_montreuil.png" alt="iut_montreuil"></div>
+            <div class="col sponsor"><img class="rounded-2" src="/images/illustrations/paris8.jpg" alt="paris8"></div>
+            <div class="col sponsor"><img class="rounded-2" src="/images/illustrations/iut_montreuil.png" alt="iut_montreuil"></div>
+            <div class="col sponsor"><img class="rounded-2" src="/images/illustrations/paris8.jpg" alt="paris8"></div>
     </section>
 </div>
