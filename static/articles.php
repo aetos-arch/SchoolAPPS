58ae52a3aeceef3c53e35d33f06070e61ed8da6e
Nicolas-Torres <sdtorres.sa@gmail.com>
2021-01-18 18:23:03 +0100

    images articles

--- static/articles.php
+++ static/articles.php
@@ -6,7 +6,7 @@
             <div class="actu card text-center">
                 <div class="card-body">
                     <h5 class="card-title">Actu 1</h5>
-                    <img class="img-fluid" src="../images/second.png">
+                    <img class="img-fluid" src="../images/illustrations/macbook.jpg">
                     <p class="card-text">Ultricies leo integer malesuada nunc. Viverra vitae congue eu consequat ac felis donec et odio. Lorem ipsum dolor sit amet consectetur adipiscing elit.
                         Tortor at auctor urna nunc id cursus metus. Etiam tempor orci eu lobortis elementum. Non quam lacus suspendisse faucibus interdum posuere lorem.
                         Cum sociis natoque penatibus et magnis. Ut diam quam nulla porttitor. Purus viverra accumsan in nisl nisi.</p>
@@ -17,7 +17,7 @@
             <div class="actu card text-center">
                 <div class="card-body">
                     <h5 class="card-title">Actu 1</h5>
-                    <img class="img-fluid" src="../images/second.png">
+                    <img class="img-fluid" src="../images/illustrations/sport.jpg">
                     <p class="card-text">Ultricies leo integer malesuada nunc. Viverra vitae congue eu consequat ac felis donec et odio. Lorem ipsum dolor sit amet consectetur adipiscing elit.
                         Tortor at auctor urna nunc id cursus metus. Etiam tempor orci eu lobortis elementum. Non quam lacus suspendisse faucibus interdum posuere lorem.
                         Cum sociis natoque penatibus et magnis. Ut diam quam nulla porttitor. Purus viverra accumsan in nisl nisi.</p>
@@ -28,7 +28,7 @@
             <div class="actu card text-center">
                 <div class="card-body">
                     <h5 class="card-title">Actu 1</h5>
-                    <img class="img-fluid" src="../images/second.png">
+                    <img class="img-fluid" src="../images/illustrations/school.jpg">
                     <p class="card-text">Ultricies leo integer malesuada nunc. Viverra vitae congue eu consequat ac felis donec et odio. Lorem ipsum dolor sit amet consectetur adipiscing elit.
                         Tortor at auctor urna nunc id cursus metus. Etiam tempor orci eu lobortis elementum. Non quam lacus suspendisse faucibus interdum posuere lorem.
                         Cum sociis natoque penatibus et magnis. Ut diam quam nulla porttitor. Purus viverra accumsan in nisl nisi.</p>
