f5fc220ce3a53b4d7217f00889bad6a0aef60432
Nicolas-Torres <sdtorres.sa@gmail.com>
2021-01-21 20:05:12 +0100

    correction chemin image

--- include/inc_products.php
+++ include/inc_products.php
@@ -2,7 +2,7 @@
     <h1>Découvrez nos produits</h1>
     <section class="row">
         <article class="products col-md-3">
-            <img class="w-25" src="/images/SchoolNet.png" alt="logo produit SchoolNet">
+            <img class="w-25" src="/images/logo/SchoolNet.png" alt="logo produit SchoolNet">
             <h2>School Net</h2>
             <p>
                 Plateforme éducative numérique destiné aux enfants (maternelle à cm2) pour les écoles, mairies et les familles.
@@ -11,7 +11,7 @@
             <a class="btn lire-plus" href="schoolnet">En savoir plus</a>
         </article>
         <article class="products col-md-3">
-            <img class="w-25" src="/images/SchoolDev.png" alt="logo produit SchoolDev">
+            <img class="w-25" src="/images/logo/SchoolDev.png" alt="logo produit SchoolDev">
             <h2>School Dev</h2>
             <p>
                 Apprendre à coder dans le langage de votre choix, quelle que soit votre âge et votre niveau.
@@ -20,7 +20,7 @@
             <a class="btn lire-plus" href="schooldev">En savoir plus</a>
         </article>
         <article class="products col-md-3">
-            <img class="w-25" src="/images/E-education.png" alt="logo produit E-education">
+            <img class="w-25" src="/images/logo/E-education.png" alt="logo produit E-education">
             <h2>E-education</h2>
             <p>
                 LA solution complète de tous les outils indispensables au fonctionnement des établissements de formation
