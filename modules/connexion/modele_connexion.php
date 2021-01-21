3f76ea2a12171fee408c7a1f574e895be606c79b
Emilie Coatelant <emiliecoatelant@outlook.fr>
2021-01-21 21:50:53 +0100

    Sécurisation du module connexion

--- modules/connexion/modele_connexion.php
+++ modules/connexion/modele_connexion.php
@@ -5,31 +5,40 @@ require_once 'config/connexion.php';
 class ModeleConnexion extends Connexion{
 
     function connexion($pseudo){
-        $selectPreparee =Connexion::$bdd->prepare('SELECT idUtilisateur,login,hashMdp,idTypeUtilisateur FROM utilisateurs 
+        try{
+            $selectPreparee =Connexion::$bdd->prepare('SELECT idUtilisateur,login,hashMdp,idTypeUtilisateur FROM utilisateurs 
             WHERE login=:pseudo');
-        $reponse = array(':pseudo' => $pseudo);
-        $selectPreparee->execute($reponse);
-        return $selectPreparee->fetchAll();
+            $reponse = array(':pseudo' => $pseudo);
+            $selectPreparee->execute($reponse);
+            return $selectPreparee->fetchAll();
+        } catch (PDOException $e) {
+        }
     }
 
     function inscription($login, $nom, $prenom, $mdp, $eFacturation, $eLivraison, $tel, $dateNaiss)
     {
-        $selectPrepareeInsert = Connexion::$bdd->prepare('
+        try{
+            $selectPrepareeInsert = Connexion::$bdd->prepare('
             INSERT INTO utilisateurs 
                 (login, nom, prenom, hashMdp, emailFacturation, emailLivraison,
                  telephone, dateNaissance, idTypeUtilisateur)
             VALUES (:login, :nom, :prenom, :mdp, :eFacturation, :eLivraison, :tel, :dateNaiss, "3")');
-        $reponse = array(':login' => $login, ':nom' => $nom, ':prenom' => $prenom, ':mdp' => password_hash($mdp, PASSWORD_DEFAULT),
-            ':eFacturation' => $eFacturation, ':eLivraison' => $eLivraison, ':tel' => $tel, ':dateNaiss' => $dateNaiss);
-        $selectPrepareeInsert->execute($reponse);
+            $reponse = array(':login' => $login, ':nom' => $nom, ':prenom' => $prenom, ':mdp' => password_hash($mdp, PASSWORD_DEFAULT),
+                ':eFacturation' => $eFacturation, ':eLivraison' => $eLivraison, ':tel' => $tel, ':dateNaiss' => $dateNaiss);
+            $selectPrepareeInsert->execute($reponse);
+        } catch (PDOException $e) {
+        }
     }
 
     function verifInscription($login){
         //Vérification de l'inscription :
-        $selectPrepareeVerif = Connexion::$bdd->prepare('
+        try{
+            $selectPrepareeVerif = Connexion::$bdd->prepare('
             SELECT idUtilisateur,login,idTypeUtilisateur FROM utilisateurs WHERE login =:login');
-        $selectPrepareeVerif->execute(array(':login' => $login));
-        return $selectPrepareeVerif->fetchAll();
+            $selectPrepareeVerif->execute(array(':login' => $login));
+            return $selectPrepareeVerif->fetchAll();
+        } catch (PDOException $e) {
+        }
     }
 
     public function loginExiste($newPseudo){
